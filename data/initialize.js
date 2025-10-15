90a0041088aecbe7239082ea17987e4b224d9968
Denalia Zhi <d.naliazhi@gmail.com>
2025-10-14 22:20:46 -0400

    feat: retrieve all expected data from API using pagination

--- data/initialize.js
+++ data/initialize.js
@@ -7,17 +7,23 @@
 const APP_TOKEN = process.env.APP_TOKEN;
 
 // Socrata query language: https://dev.socrata.com/docs/queries/
-const soql = "SELECT%20name%20SEARCH%20%22fountain%22";
+const QUERY =
+  "SELECT%20name%2C%20borough%2C%20parkname%2C%20location%2C%20dedicated%2C%20architect%2C%20categories%2C%20x%2C%20y%20SEARCH%20%22fountain%22";
 
-async function getFountainsFromAPI() {
+// Anything > 40 results doesn't return full output
+// String gets cut off and causes json.parse syntaxError due to unclosed quote
+const RESULTS_PER_PAGE = 40;
+// Pagination to get all expected 252 results
+const PAGE_COUNT = 7;
+
+async function getFountainsFromAPI(page, results) {
   try {
     const resp = await fetch(
-      `https://data.cityofnewyork.us/api/v3/views/6rrm-vxj9/query.json?pageNumber=1&pageSize=10&app_token=${APP_TOKEN}&query=${soql}`
+      `https://data.cityofnewyork.us/api/v3/views/6rrm-vxj9/query.json?pageNumber=${page}&pageSize=${RESULTS_PER_PAGE}&app_token=${APP_TOKEN}&query=${QUERY}`
     );
     if (!resp.ok)
       throw new Error(`API request failed: ${resp.status}, ${resp.statusText}`);
 
-    let results = [];
     for await (const chunk of resp.body) {
       const decoder = new TextDecoder("utf-8");
       const str = decoder.decode(chunk);
@@ -32,8 +38,13 @@ async function getFountainsFromAPI() {
   }
 }
 
-function main() {
-  getFountainsFromAPI();
+async function main() {
+  let results = [];
+  for (let page = 1; page <= PAGE_COUNT; page++) {
+    console.log(`Getting page ${page}`);
+    await getFountainsFromAPI(page, results);
+  }
+  console.log(results.length);
 }
 
 main();
