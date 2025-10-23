661a970bbf0343c455b54ce556eac7658ba31a29
Denalia Zhi <d.naliazhi@gmail.com>
2025-10-23 00:02:11 -0400

    style: add icons, general prettification style: tabs in user dashboard

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ const app = express();
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const assetsPath = path.join(__dirname, "public");
-app.use(express.static(assetsPath));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public/icons")));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
